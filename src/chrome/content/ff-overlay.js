0a6096ccf8108660e1ae1bb242005530e28e4aed
unknown <normal@.(none)>
2011-03-21 06:47:23 -0500

    changed all refs to WindowNamer.Overlay as part of refactor

--- src/chrome/content/ff-overlay.js
+++ src/chrome/content/ff-overlay.js
@@ -1,11 +1,11 @@
-windownamer.onFirefoxLoad = function(event) {
+WindowNamer.Overlay.onFirefoxLoad = function(event) {
   document.getElementById("contentAreaContextMenu")
-          .addEventListener("popupshowing", function (e){ windownamer.showFirefoxContextMenu(e); }, false);
+          .addEventListener("popupshowing", function (e){ WindowNamer.Overlay.showFirefoxContextMenu(e); }, false);
 };
 
-windownamer.showFirefoxContextMenu = function(event) {
+WindowNamer.Overlay.showFirefoxContextMenu = function(event) {
   // show or hide the menuitem based on what the context menu is on
-  document.getElementById("context-windownamer").hidden = gContextMenu.onImage;
+  document.getElementById("context-WindowNamer.Overlay").hidden = gContextMenu.onImage;
 };
 
-window.addEventListener("load", function () { windownamer.onFirefoxLoad(); }, false);
+window.addEventListener("load", function () { WindowNamer.Overlay.onFirefoxLoad(); }, false);
